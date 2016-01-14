95d3b77455e30741c964107423c9e729a8e1aaf5
Joey Arnstein and Matt Rosanio <mmrosani@syr.edu>
2016-01-14 14:52:33 -0800

    more content and JQuery functionality

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,25 @@ $(document).ready(function(){
     $("#drgn").toggle();
   });
 
+  $("#listbtn").click(function(){
+    $('#apnd').append("<p class='new'>Text!</p>");
+  });
+
+  $("#rmvbtn").click(function(){
+    $("#apnd").children(".new").first().remove();
+  });
+
+  $("#clr").click(function(){
+    $("#clrchng").css('background-color', 'yellow');
+  });
+
+  $("#clr").dblclick(function(){
+    $("#clrchng").css('background-color', 'white')
+  });
+
+  $('#blanks form').submit(function(event) {
+    var nameInput = $('input#name2').val();
+    $('.formshow').text(nameInput);
+    event.preventDefault();
+  });
 });
